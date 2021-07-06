3ccaf6d11a6ab051ca5c75ad01dacc8452e5b28b
FinnWestern <77283784+FHafting@users.noreply.github.com>
2021-07-06 15:55:34 -0400

    Create Static_Sample_Number.ino

---
+++ Examples/Static_Sample_Number/Static_Sample_Number.ino
@@ -0,0 +1,36 @@
+#include <Median.h>
+
+float values[] = {33.2, 44.1, 23.2, 11, 13, 12.3, 12.5, 67.2, 45.2, 78.7, 67.5, 12.3, 14.5, 89.6, 90.3, 24.4, 27.5};
+float medianVal;
+
+int windowSize = 5;
+
+//declare filter object as myMedian with a window size of 5 
+Median myMedian(windowSize);
+
+void setup() {
+  Serial.begin(9600);
+  for(int i = 0; i<(sizeof(values)/sizeof(float)); i++){
+    myMedian.addValue(values[i]);     //add new value to sampling buffer
+
+    //wait until first 5 values are in buffer before calculating median
+    if(i >= windowSize){
+      
+      medianVal = myMedian.getMedian(); //get new median value
+      
+      Serial.print("Added value: ");
+      Serial.print(values[i]);
+      Serial.print(" Median: ");
+      Serial.println(medianVal);
+      delay(100);
+    }
+  }
+  
+  //free dynamic memory
+  myMedian.freeMemory();
+
+}
+
+void loop(){
+}
+
