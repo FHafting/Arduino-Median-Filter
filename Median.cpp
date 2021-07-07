56f32f18e651922f76fdf69e15c347cb0c7c1f98
Finn Hafting <77283784+FHafting@users.noreply.github.com>
2021-07-07 15:59:17 -0300

    Removed need for dynamic memory

--- Median.cpp
+++ Median.cpp
@@ -8,35 +8,33 @@
 
 Median::Median(int size)
 {
-  window_size = size;
+  _size = size;
   median_value = 0;
 
-  //initialize dynamic memory arrays to zero
-  unsorted = (float *)calloc(window_size, sizeof(float));
-  sorted = (float * )calloc(window_size, sizeof(float));
+  for(int i = 0; i < _size; i++){
+    unsorted[i] = 0;
+    sorted[i] = 0;
+  }
 }
 
 void Median::addValue(float value)
 {
   //add new value to end of array
-  for(int i = 1; i < window_size; i++){
+  for(int i = 1; i < _size; i++){
     unsorted[i - 1] = unsorted[i];
   }
-  unsorted[window_size - 1] = value;
+  unsorted[_size - 1] = value;
 
   //copy array over to be sorted
-  for(int i =0; i < window_size; i++){
+  for(int i = 0; i < _size; i++){
     sorted[i] = unsorted[i];
   }
 
   //sort array in ascending order (bubble sort)
-  for(int k = 0; k < (window_size - 1); k++){
-    for(int i = 1; i < window_size; i++){
+  for(int k = 0; k < (_size - 1); k++){
+    for(int i = 1; i < _size; i++){
       if(sorted[i-1] > sorted[i]){
-        float temp;
-        temp = sorted[i-1];
-        sorted[i-1] = sorted[i];
-        sorted[i] = temp;
+        swapValues(&sorted[i-1], &sorted[i]);
       }
     }
   }
@@ -44,12 +42,14 @@ void Median::addValue(float value)
 
 float Median::getMedian()
 {
-  int medianPos = (int)((window_size - 1) / 2);
+  int medianPos = (int)((_size - 1) / 2);
   return sorted[medianPos];
 }
 
-void Median::freeMemory()
+void Median::swapValues(float *leftVal, float *rightVal)        // pass in pointers to modify original values
 {
-  free(unsorted);
-  free(sorted);
+    float temp;       // create placeholder for temporary storage
+    temp = *leftVal;
+    *leftVal = *rightVal;       // swap values using pointers to modify original values
+    *rightVal = temp;
 }
